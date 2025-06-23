d15aea313cf8bd464eed0a2f97d91fa92b76334d
JORGE AGUSTIN EROSA HERRERA <jasonruggeti@gmail.com>
2025-06-23 16:52:12 +0000

    added encounters table to track

---
+++ models/marts/fct_encounters_summary.sql
@@ -0,0 +1,54 @@
+with encounters as (
+    select * from {{ ref('stg_encounters') }}
+),
+
+diagnosis_counts as (
+    select
+        encounter,
+        count(*) as diagnosis_count
+    from {{ ref('stg_conditions') }}
+    group by encounter
+),
+
+procedure_counts as (
+    select
+        encounter,
+        count(*) as procedure_count
+    from {{ ref('stg_procedures') }}
+    group by encounter
+),
+
+medication_counts as (
+    select
+        encounter,
+        count(*) as medications_count
+    from {{ ref('stg_medications') }}
+    group by encounter
+),
+
+claim_totals as (
+    select
+        encounter,
+        sum(total_claim_cost) as total_claim_amount
+    from {{ ref('stg_claims') }}
+    group by encounter
+)
+
+select
+    e.id as encounter_id,
+    e.patient as patient_id,
+    e.encounterclass as encounter_type,
+    e.startdate as start_time,
+    e.stopdate as end_time,
+    datediff(day, e.startdate, e.stopdate) as length_of_stay,
+
+    coalesce(diagnosis_count, 0) as diagnosis_count,
+    coalesce(procedure_count, 0) as procedure_count,
+    coalesce(medications_count, 0) as medications_count,
+    coalesce(total_claim_amount, 0.0) as total_claim_amount
+
+from encounters e
+left join diagnosis_counts d on e.id = d.encounter
+left join procedure_counts p on e.id = p.encounter
+left join medication_counts m on e.id = m.encounter
+left join claim_totals c on e.id = c.encounter
