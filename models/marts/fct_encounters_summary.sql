3ec6b9b5754e4436c118589143888ab95afa45e7
JORGE AGUSTIN EROSA HERRERA <jasonruggeti@gmail.com>
2025-06-23 17:16:22 +0000

    .

--- models/marts/fct_encounters_summary.sql
+++ models/marts/fct_encounters_summary.sql
@@ -38,17 +38,17 @@ select
     e.encounter_id as encounter_id,
     e.patient as patient_id,
     e.encounterclass as encounter_type,
-    e.startdate as start_time,
-    e.stopdate as end_time,
-    datediff(day, e.startdate, e.stopdate) as length_of_stay,
+    e.start_time as start_time,
+    e.end_time as end_time,
+    datediff(day, e.start_time, e.end_time) as length_of_stay,
 
     coalesce(diagnosis_count, 0) as diagnosis_count,
     coalesce(procedure_count, 0) as procedure_count,
     coalesce(medications_count, 0) as medications_count,
-    coalesce(total_claim_amount, 0.0) as total_claim_amount
+    coalesce(total_outstanding_amount, 0.0) as total_outstanding_amount
 
 from encounters e
 left join diagnosis_counts d on e.encounter_id = d.encounter
 left join procedure_counts p on e.encounter_id = p.encounter
 left join medication_counts m on e.encounter_id = m.encounter
-left join claim_totals c on e.encounter_id = c.encounter
+left join claim_totals c on e.encounter_id = c.encounter_id
